3abc6347228c29ebeb6a9c27b728c7075a400bf0
fkropfhamer <fabian.kropfhamer@gmail.com>
2024-02-10 12:08:06 +0100

    add morse parsing.

--- lib/screens/transmit.dart
+++ lib/screens/transmit.dart
@@ -1,4 +1,155 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+const morseMap = {
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
+};
+
+const timeUnit = 200;
+const dit = 1 * timeUnit;
+const dah = 2 * timeUnit;
+const interElement = 1 * timeUnit;
+const shortGap = 3 * timeUnit;
+const mediumGap = 5 * timeUnit;
+
+
+
+enum TransmitType {
+  pause, send
+}
+
+enum TransmitClass {
+  dit, dah, interElement, shortGap, mediumGap;
+
+  @override
+  String toString() {
+    switch(this) {
+
+      case TransmitClass.dit:
+        return '.';
+      case TransmitClass.dah:
+        return '-';
+      case TransmitClass.interElement:
+      case TransmitClass.shortGap:
+      case TransmitClass.mediumGap:
+        return '';
+    }
+  }
+}
+
+class Transmit {
+  const Transmit(this.duration, this.type);
+
+  final int duration;
+  final TransmitType type;
+
+  TransmitClass _getPauseClass() {
+    if (duration > mediumGap) {
+      return TransmitClass.mediumGap;
+    }
+
+    if (duration > shortGap) {
+      return TransmitClass.shortGap;
+    }
+
+    return TransmitClass.interElement;
+  }
+
+  TransmitClass  _getSendClass() {
+    if (duration > dah) {
+      return TransmitClass.dah;
+    }
+
+    return TransmitClass.dit;
+  }
+
+  TransmitClass getClass() {
+    if (type == TransmitType.pause) {
+      return _getPauseClass();
+    }
+
+    return _getSendClass();
+  }
+}
+
+String parseLetter(List<Transmit> transmits) {
+  final ts = transmits.map((t) => t.getClass().toString()).join();
+  final letter = morseMap[ts];
+
+  if (letter != null) {
+    return letter;
+  }
+
+  return "?";
+}
+
+String parseWord(List<Transmit> transmits) {
+  List<List<Transmit>> letters = [[]];
+
+  for (Transmit t in transmits) {
+    if (t.type == TransmitType.pause && t.getClass() == TransmitClass.shortGap) {
+      letters.add([]);
+      continue;
+    }
+
+    letters.last.add(t);
+  }
+
+  return letters.map((l) => parseLetter(l)).join();
+}
+
+String parseMorse(List<Transmit> transmits) {
+  List<List<Transmit>> words = [[]];
+
+  var ts = transmits.where((t) => t.getClass() != TransmitClass.interElement);
+
+  for (Transmit t in ts) {
+    if (t.type == TransmitType.pause && t.getClass() == TransmitClass.mediumGap) {
+      words.add([]);
+      continue;
+    }
+
+    words.last.add(t);
+  }
+
+  return words.map((w) => parseWord(w)).join(" ");
+}
 
 class TransmitScreen extends StatefulWidget {
   const TransmitScreen({super.key});
@@ -8,22 +159,25 @@ class TransmitScreen extends StatefulWidget {
 }
 
 class _TransmitScreenState extends State<TransmitScreen> {
-  var transmits = <int>[];
+  var transmits = <Transmit>[];
   var ts = DateTime.now().millisecondsSinceEpoch;
+  var text = "";
 
   final _scrollController = ScrollController();
 
-  void a() {
+  void addTransmit(TransmitType t) {
 
     setState(() {
       final now = DateTime.now().millisecondsSinceEpoch;
-      transmits.add(now - ts);
+      final diff = min(now - ts, 3000);
+      final transmit = Transmit(diff, t);
+
+      transmits.add(transmit);
       ts = now;
+      text = parseMorse(transmits);
 
       _scrollController.animateTo(_scrollController.position.maxScrollExtent, duration: const Duration(microseconds: 500), curve: Curves.easeOut);
     });
-
-    print(transmits);
   }
 
   @override
@@ -37,6 +191,7 @@ class _TransmitScreenState extends State<TransmitScreen> {
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Text(text),
               SizedBox(
                 height: 50,
                width: double.infinity,
@@ -46,24 +201,23 @@ class _TransmitScreenState extends State<TransmitScreen> {
                    itemCount: transmits.length,
                    scrollDirection: Axis.horizontal,
                    itemBuilder: (BuildContext context, int index) {
-                     final color = index % 2 == 0 ? Colors.green : Colors.grey;
+                      final transmit = transmits[index];
+                     final color = transmit.type == TransmitType.pause ? Colors.grey : Colors.green;
 
                      return Container(
                        height: 50,
-                       width: transmits[index] / 10,
+                       width: transmit.duration / 10,
                        color: color,
-                       child: Center(child: Text('${transmits[index]}')),
+                       child: Center(child: Text('${transmit.getClass()}')),
                      );
                    })
               ),
               InkResponse(
                   onTapDown: (t) {
-                    print("down");
-                    a();
+                    addTransmit(TransmitType.pause);
                   },
                   onTapUp: (t) {
-                    print("up");
-                    a();
+                    addTransmit(TransmitType.send);
                   },
                   child: Container(
                       width: 100.0,
